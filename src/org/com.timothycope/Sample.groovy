b369876db08df4fcca9d29aa0e3d135b7234a131
Timothy Cope <kirbycope@gmail.com>
2023-06-20 17:52:02 -0700

    Update Sample.groovy

--- src/org/com.timothycope/Sample.groovy
+++ src/org/com.timothycope/Sample.groovy
@@ -4,6 +4,7 @@ class Sample {
     /**
     * Echoes 'Hello, World!'
     * @param context - The `config[:]` passed in as `this` from the calling method.
+    * @param text - The text to echo.
     */
     static def Foo(context, text) {
         context.sh "echo ${text}"
